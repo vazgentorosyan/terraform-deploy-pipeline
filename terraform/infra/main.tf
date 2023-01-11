9670545af04e47bece1b4c12f3ea1e90ff809f5f
Vazgen Torosyan <vazgentorosyan@Vazgens-MacBook-Pro.local>
2023-01-11 19:02:07 +0400

    added deploy workflow

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "this" {
+  name = "test-sg"
+  description = "just a test sg"
+  tags = {
+    Name = "TEST-SG-TERRAFORM"
+  }
+}
