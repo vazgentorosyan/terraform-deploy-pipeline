c4cd8c324a2e2b0a46d0ea8643e236d1ae07162e
Vazgen Torosyan <vazgentorosyan@Vazgens-MacBook-Pro.local>
2023-01-12 16:25:54 +0400

    added backend config

--- terraform/infra/versions.tf
+++ terraform/infra/versions.tf
@@ -6,4 +6,10 @@ terraform {
       version = ">= 4.0.0"
     }
   }
+  backend "s3" {
+    bucket         = "terraform-tfstate-test-01"
+    key            = "TERRAFORM-FOLDER/vpc-state.tfstate"
+    dynamodb_table = "TF-STATE-LOCK"
+    region         = "us-east-1"
+  }
 }
