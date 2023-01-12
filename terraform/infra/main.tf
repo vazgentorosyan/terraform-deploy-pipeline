cbe07c0788dec5ff2133c15088560f090108f11c
Vazgen Torosyan <vazgentorosyan@Vazgens-MacBook-Pro.local>
2023-01-12 21:43:22 +0400

    fixed duplicated versions

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -1,17 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "= 4.0.0"
-    }
-  }
-  backend "s3" {
-    bucket         = "terraform-tfstate-test-01"
-    key            = "TERRAFORM-FOLDER/vpc-state.tfstate"
-    dynamodb_table = "TF-STATE-LOCK"
-    region         = "us-east-1"
-  }
-}
+
 provider "aws" {
   region = var.aws_region
 }
