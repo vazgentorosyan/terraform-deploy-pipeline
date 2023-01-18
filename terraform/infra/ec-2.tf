2e004d2842cca16748e29549228b31ac7d27d3f9
Vazgen Torosyan <vazgentorosyan@Vazgens-MacBook-Pro.local>
2023-01-18 14:32:37 +0400

    added super important sq

--- terraform/infra/ec-2.tf
+++ terraform/infra/ec-2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "public_ec2" {
   security_groups = [aws_security_group.public_sg.id]
 
   tags = {
-    Name = upper("${var.product_name}-PUBLIC-EC2-COOL")
+    Name = upper("${var.product_name}-PUBLIC-EC2")
   }
   lifecycle {
     ignore_changes = [security_groups, tags]
---
+++ terraform/infra/s3.tf
@@ -0,0 +1,6 @@
+resource "aws_security_group" "super_important_group" {
+  description = "this is very important group"
+  tags = {
+    "Name" = "SUPER IMPORTANT KIZICH"
+  }
+}
