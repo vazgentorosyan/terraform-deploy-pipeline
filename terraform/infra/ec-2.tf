051bc551c3f8c3a6ce00e625330de5cb839a0033
Vazgen Torosyan <vazgentorosyan@gmail.com>
2023-03-22 11:47:54 +0400

    added depends-on

--- terraform/infra/ec-2.tf
+++ terraform/infra/ec-2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "public_ec2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.PublicSubnet.0.id
-  key_name = aws_key_pair.key_pair.key_name
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.PublicSubnet.0.id
+  key_name                    = aws_key_pair.key_pair.key_name
   associate_public_ip_address = true
-  security_groups = [aws_security_group.public_sg.id]
+  security_groups             = [aws_security_group.public_sg.id]
 
   tags = {
     Name = upper("${var.product_name}-PUBLIC-EC2")
@@ -12,13 +12,14 @@ resource "aws_instance" "public_ec2" {
   lifecycle {
     ignore_changes = [security_groups]
   }
+  depends_on = [aws_nat_gateway.nat_gateway]
 }
 
 resource "aws_instance" "nat_ec2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.NatSubnet.0.id
-  key_name = aws_key_pair.key_pair.key_name
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.NatSubnet.0.id
+  key_name        = aws_key_pair.key_pair.key_name
   security_groups = [aws_security_group.nat_sg.id]
 
   tags = {
@@ -28,4 +29,5 @@ resource "aws_instance" "nat_ec2" {
   lifecycle {
     ignore_changes = [security_groups, tags]
   }
+  depends_on = [aws_nat_gateway.nat_gateway]
 }
